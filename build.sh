4cfa3160dc40553b9885e786bc77760811d57f3c
Jacob Malloy <sjakem@att.net>
2022-08-11 21:51:12 -0400

    build on linux

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
-INCLUDE_FLAGS="$(freetype-config --cflags) -Iglad/include"
-LIB_FLAGS="$(freetype-config --libs) -ldl"
+#!/usr/bin/env bash
+INCLUDE_FLAGS="$(pkg-config freetype2 --cflags) -Iglad/include"
+LIB_FLAGS="$(pkg-config freetype2 --libs) -ldl"
 
 
 
-if [[ $(uname)=="Darwin" ]]; then
+if [[ $(uname) == "Darwin" ]]; then
     GL_LIB="-L${HOME}/.local/lib -framework OpenGL -lglfw3 -framework Cocoa -framework IOKit -framework CoreVideo"
 else
     GL_LIB="-lGL -lglfw"
@@ -12,4 +13,4 @@ fi
 LIB_FLAGS="${LIB_FLAGS} ${GL_LIB}"
 
 
-gcc -g ${INCLUDE_FLAGS} ${LIB_FLAGS} -o SIG src/main.c src/glad.c
+gcc -g ${INCLUDE_FLAGS} -o SIG src/main.c src/glad.c ${LIB_FLAGS}
